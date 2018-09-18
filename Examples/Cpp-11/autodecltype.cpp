09ac94a82ea696c72f4b8a1eb5e7bce33c5fdd8c
Vandit Maniar <43340512+VanditManiar@users.noreply.github.com>
2018-09-18 14:51:15 +0530

    Create autodecltype.cpp

---
+++ Examples/Cpp-11/autodecltype.cpp
@@ -0,0 +1,26 @@
+/*
+  Author : Vandit Maniar
+
+  Easy-Way-Of-Exploring-Cpp/ModernCpp
+  C++ 11 Example : AutoDecltype
+
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    auto i = 3;
+    decltype(i) a;       /* a has now the type of i */
+
+    auto l = {1, 2, 3};  /* l has now the type initializer_list */
+    auto v = vector<int>({4, 5, 6});
+
+    for(auto x : v)
+        cout << x << endl;
+        
+    return 0;
+}
