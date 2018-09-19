298f505fc17715f47e487b7b511cb1241798f2e1
Vandit Maniar <43340512+VanditManiar@users.noreply.github.com>
2018-09-19 23:50:55 +0530

    Create UserDefinedLiterals.cpp

---
+++ Examples/Cpp-11/UserDefinedLiterals.cpp
@@ -0,0 +1,36 @@
+/*
+  Author : Vandit Maniar
+
+  Easy-Way-Of-Exploring-Cpp/ModernCpp
+  C++ 11 Example : UserDefinedLiterals
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+class Inch*
+{
+public:
+    explicit Inch() : Inch(0) { }
+    explicit Inch(double value) : Value(value) { }
+
+    double toCentimeter() const {
+        return Value * 2.54;
+    }
+
+private:
+    double Value = 0;
+};
+
+Inch operator "" _inch(long double arg)
+{
+    return Inch(arg);
+}
+
+int main()
+{
+    cout << (1.5_inch).toCentimeter() << " centimeter" << endl;
+    return 0;
+}
